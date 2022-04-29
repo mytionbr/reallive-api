aea9c2b934c9ca06ea44ac04925eddf3fa726ff3
matheus jesus <matheuss.jesus2017@gmail.com>
2022-04-29 17:27:28 -0300

    feat: generate title for single chat

--- src/chat-room/chat-room.module.ts
+++ src/chat-room/chat-room.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { DynamooseModule } from 'nestjs-dynamoose';
 import { MessageModule } from 'src/message/message.module';
+import { UserModule } from 'src/user/user.module';
 import { ChatRoomResolver } from './chat-room.resolver';
 import { ChatRoomService } from './chat-room.service';
 import { ChatRoomSchema } from './schema/chat-room.schema';
@@ -14,6 +15,7 @@ import { ChatRoomSchema } from './schema/chat-room.schema';
       },
     ]),
     MessageModule,
+    UserModule,
   ],
   providers: [ChatRoomResolver, ChatRoomService],
 })
--- src/chat-room/chat-room.service.ts
+++ src/chat-room/chat-room.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel, Model } from 'nestjs-dynamoose';
 import { CreateChatRoomInput } from './dto/create-chat-room.input';
-import { ChatRoom, ChatRoomKey } from './model/chat-room.entity';
+import {
+  ChatRoom,
+  ChatRoomKey,
+  ChatRoomTypeEnum,
+} from './model/chat-room.entity';
 import { v4 as uuidv4 } from 'uuid';
 import { Condition } from 'dynamoose';
 import { MessageService } from 'src/message/message.service';
 import { ChatRoomMessagesOutput } from './dto/chat-room-messages.output';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class ChatRoomService {
@@ -13,6 +18,7 @@ export class ChatRoomService {
     @InjectModel('chatRoom')
     private readonly chatRoomModel: Model<ChatRoom, ChatRoomKey>,
     private messageService: MessageService,
+    private userService: UserService,
   ) {}
 
   async createChatRoom(data: CreateChatRoomInput): Promise<ChatRoom> {
@@ -27,8 +33,8 @@ export class ChatRoomService {
   async findByUserId(userId: string): Promise<ChatRoom[]> {
     const condition = new Condition('usersId').contains(userId);
     const result: ChatRoom[] = await this.chatRoomModel.scan(condition).exec();
-
-    return result;
+    const chatRooms = await this.parseChatTitle(result, userId);
+    return chatRooms;
   }
 
   async findById(chatRoomId: string): Promise<ChatRoom> {
@@ -48,5 +54,23 @@ export class ChatRoomService {
     };
 
     return chatRoomMessagesOutput;
+  }
+
+  private async parseChatTitle(
+    chatRooms: ChatRoom[],
+    currentUserId: string,
+  ): Promise<ChatRoom[]> {
+    const chatRoomResult = await Promise.all(
+      chatRooms.map(async (chat) => {
+        if (chat.type === ChatRoomTypeEnum.SINGLE) {
+          const otherUserId = chat.usersId.find((id) => id !== currentUserId);
+          const otherUser = await this.userService.findOne({ id: otherUserId });
+          chat.title = otherUser.nickname;
+        }
+        return chat;
+      }),
+    );
+    console.log(chatRoomResult);
+    return chatRoomResult;
   }
 }
